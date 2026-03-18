c4054c2e6fa52171243ce8ef28dd8d7f99424e00
Laurent VEYSSIER <lveyssier@gmail.com>
2026-03-18 12:55:07 +0100

    chore: modified azure deployment approach

--- startup.sh
+++ startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # for Azure Web App
+# Activate the virtual environment that uv created
+source antenv/bin/activate
+# start up command
 set -e   # if part of the script fails → script exits immediately
 python3 run_agents.py &
 exec python3 frontend_app.py
